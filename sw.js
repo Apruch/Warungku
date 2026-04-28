96a22be69e467eeca3f9201d4d1c6e2de1d31490
Apruch <anugrahridho545@gmail.com>
2026-04-28 22:15:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Base path: /Warungku/
 // ═══════════════════════════════════════════════════════
 
-const CACHE_NAME = 'warungku-v7';
+const CACHE_NAME = 'warungku-v37';
 const BASE = '/Warungku/';
 
 // Semua file yang akan di-cache untuk akses offline
